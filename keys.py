762260c4764a189da9754ce3f8dfc5c2091cf930
Victor <victormuia8@gmail.com>
2021-07-20 17:21:31 +0300

    Second commit

---
+++ keys.py
@@ -0,0 +1,7 @@
+business_shortCode = "174379"  # Lipa Na Mpesa Shortcode
+phone_number = 254710295224
+lipa_na_mpesa_passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
+consumer_key = "bYTqy2EX0eZXmMnEAgRxeJYv19EDCF12"
+consumer_secret = "mAWQfSQlqC28udgb"
+# shortcode = config("SHORTCODE")
+# test_msisdn = config("TEST_MSISDN")
--- lipanampesa.py
+++ lipanampesa.py
@@ -1,1 +1,37 @@
-print('Hello World')
+import requests
+from requests.auth import HTTPBasicAuth
+
+from utils import get_timestamp
+from encode import generate_password
+from access_token import generate_access_token
+import keys
+
+def lipa_na_mpesa():
+    formatted_time = get_timestamp()
+    decoded_password = generate_password(formatted_time)
+    access_token = generate_access_token()
+
+    api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+
+    headers = {"Authorization": "Bearer %s" % access_token}
+
+    request = {
+        "BusinessShortCode": keys.business_shortCode,
+        "Password": decoded_password,
+        "Timestamp": formatted_time,
+        "TransactionType": "CustomerPayBillOnline",
+        "Amount": "1",
+        "PartyA": keys.phone_number,
+        "PartyB": keys.business_shortCode,
+        "PhoneNumber": keys.phone_number,
+        "CallBackURL": "https://sandbox.safaricom.co.ke/mpesa/",
+        "AccountReference": "testing",
+        "TransactionDesc": "Pay School Fees",
+    }
+
+    response = requests.post(api_url, json=request, headers=headers)
+
+    print(response.text)
+
+
+lipa_na_mpesa()
